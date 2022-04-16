76fb6cc6b5db6c9313cdb4c243c8d4026d97a45c
sh6986 <okok960411@gmail.com>
2022-04-16 17:16:47 +0900

    Schema & Model

---
+++ src/models/User.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+// 첫번째 인자로 객체를 받고 (객체안의 key value 값 포함), 두번째는 옵션 정보
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String, 
+        required: true
+    },
+    name: {
+        first: { 
+            type: String, 
+            required: true
+        },
+        last: { 
+            type: String, 
+            required: true 
+        }
+    },
+    age: Number,
+    // age: {type: Number} 와 동일한데 필수가 아니면 이렇게 짧게 작성 가능
+    email: String
+}, {
+    timestamps: true
+    // 데이터 생성시 생성시간 createdAt을 만들고 수정시간 updateAt을 수정해준다.
+});
+
+const User = mongoose.model('user', UserSchema);
+// users 라는 이름으로 컬렉션이 생성된다.
+
+module.exports = { User };
--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const { User } = require('./models/User');
+
 const dotenv = require('dotenv');
 dotenv.config();
 
